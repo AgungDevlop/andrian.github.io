68c27e0b100bb73bc865fc063a4994e328e247f2
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2024-12-19 05:02:01 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -6,10 +6,8 @@ export function Download() {
 
   // Array of URLs for pop-under links
   const randomUrls = [
-    "https://so-gr3at3.com/go/1237187",
-    "https://malakingannets.com/ic4wSTmH5JgaK77X/94691",
-    "https://meowadvertising.com/hc70ax5ct2?key=7df760c08ecfe3653c332fbdce13d42a",
-    "https://superficial-work.com/ba3RV.0YPk3Xp/v/b/mOVsJHZqDV0Y0KO/DVQWzkOvD/MK3pLvT/QJ4JNmDyM/4MMozHgS"
+    "https://aliveprompt.com/b_3/V.0/Po3/p/vRbdmlV/JVZ/Dp0R1VOjTiIFzGN/TkU/y/L/TmUX5SMKjaMM1BNmTEMq",
+    "https://luglawhaulsano.net/4/8673906"
   ];
 
   const handleDownload = () => {
