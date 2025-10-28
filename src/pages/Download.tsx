4cfb33f649fc8adfe23b0d1e2d128aeb695257ca
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-10-28 09:53:39 +0800

    Reorder and add URLs in randomUrls array

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -5,9 +5,10 @@ export function Download() {
   const videoTitle = sessionStorage.getItem('videoTitle'); // Get video title from session storage
 
 const randomUrls = [
-    'https://adclickad.com/get/?spot_id=6089412&cat=25&subid=2067093145',
-    'https://enviousgarbage.com/HE9TFh',
     'https://otieu.com/4/10055984',
+    'https://enviousgarbage.com/HE9TFh',
+    'https://aviatorreproducesauciness.com/2082665',
+    'https://adclickad.com/get/?spot_id=6089412&cat=25&subid=2067093145',
     'https://viikqoye.com/dc/?blockID=388556'
 ];
   
